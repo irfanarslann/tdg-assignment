8c51c34487cd5c40aaf553f7b24b02cd235f445e
samSepi0l <irfanarslaann@gmail.com>
2021-06-30 18:39:21 +0300

    Initial Commit

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const app = express();
 app.use("/api/data", require("./routes/wave"));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "www/build")));
+  app.use(express.static(path.join(__dirname, "client/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "www/build", "index.html"));
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
 }
 
